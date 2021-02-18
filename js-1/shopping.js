1c061db155b79f5455959555c7591ec7f65a1d74
radhey-sahaj <65949892+radhey-sahaj@users.noreply.github.com>
2021-02-18 11:56:40 +0530

    Create shopping.js

---
+++ js-1/shopping.js
@@ -0,0 +1,22 @@
+data = [
+  ['apple', 'Fruit', 'isEdible', 40],
+  ['Lays', 'Processed Food', 'isEdible', 10],
+  ['Pen', 'Stationary', 'isNotEdible', 11],
+  ['Book', 'Stationary', 'isNotEdible', 111],
+  ['Banana', 'fruit', 'isEdible', 14],
+  ['Peanut Butter', 'Processed Food', 'isEdible', 200],
+  ['Oven', 'Appliance', 'isNotEdible', 1300],
+  ['Air Conditioner', 'Applicance', 'isNotEdible', 13300],
+  ['Drill', 'Power Tool', 'isNotEdible', 2200],
+];
+
+
+// Create a shopping items
+// result = [ { name: 'apple', category: 'Fruit', isEdible: true, price: 40 }, ... ]
+function createListOfObjects(data) {}
+
+//sort by given parameter. sorting Parameter eg: 'category' | 'price' etc
+function sortList(sortingParameter)
+
+//return list of items by category eg: 'Fruit' | 'Appliance'
+function selectByCatergory(category)
