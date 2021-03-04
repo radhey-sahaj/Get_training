537e56a92a9143fb3666822eb174154340744c0f
radhey-sahaj <65949892+radhey-sahaj@users.noreply.github.com>
2021-03-04 08:51:08 +0530

    Add files for js2 excersises

---
+++ js2/countConsonants.js
@@ -0,0 +1,16 @@
+// For help pls look into
+//   - Object.assign() to copy multiple objects into another object
+
+/*
+ * IMPORTANT INSTRUCTIONS:
+ *   - Do not use loops
+ *   - Do not use let, var
+ * Given a sentence as input, return an object with the count of consonants in the sentence
+ * @param {string} sentence
+ *
+ * eg: input: "bacd aio palleted"
+ *     output: { b: 1, c: 1, d: 3, p: 1, l: 2, t:1}
+ */
+function countConsonants(sentence) {}
+
+module.exports = countConsonants;
